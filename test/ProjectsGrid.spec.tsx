a7e8614b946a0dc432c611b4ba738212f152b9b4
Ghania <Ghania@GHANIA>
2020-08-25 01:47:39 +0200

    Add basic test suite

---
+++ test/ProjectsGrid.spec.tsx
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+import ProjectsGrid from "../src/components/ProjectsGrid";
+
+describe("Projects Grid", () => {
+    it("should sort projects on creation date", () => {
+        expect("1").to.be.equal("1");
+    })
+})
