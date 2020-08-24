aeb1a35298ebbcecc04018b9a9bc533ad9a2e771
Ghania <Ghania@GHANIA>
2020-08-24 20:59:51 +0200

    add grid to display cards

---
+++ src/components/ProjectsGrid.tsx
@@ -0,0 +1,34 @@
+import React, { FunctionComponent } from 'react';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import { ProjectCard } from './ProjectCard';
+import {Projects} from '../services/useProjectsService';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    }
+  }),
+);
+
+const ProjectsGrid: FunctionComponent<Projects> = ({results}) => {
+    const classes = useStyles();
+    debugger;
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={2} direction="row">
+            {
+                results && results.map((project: any) => (
+                    <Grid item xs={6} sm={3}>
+                    <ProjectCard key={project.id} data={project} />
+                    </Grid>
+                ))
+            }
+      </Grid>
+    </div>
+  );
+}
+
+export default ProjectsGrid;
