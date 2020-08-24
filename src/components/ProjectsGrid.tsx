3caeed39c208b41c2b10b50980dcdd852b589924
Ghania <Ghania@GHANIA>
2020-08-25 00:41:49 +0200

    Sort on date

--- src/components/ProjectsGrid.tsx
+++ src/components/ProjectsGrid.tsx
@@ -1,9 +1,9 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import { ProjectCard } from './ProjectCard';
 import {Projects} from '../services/useProjectsService';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -13,20 +13,36 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
-const ProjectsGrid: FunctionComponent<Projects> = ({results}) => {
-    const classes = useStyles();
+const moment = require('moment');
+var _ = require('lodash');
+const sortArray = (projects: Projects, sortProperty: string) => {
+  return _.orderBy(projects, function(project: any) {
+    return new moment(project.creationDate)
+  }, [sortProperty]);
+}
 
+const ProjectsGrid: FunctionComponent<Projects> = ({results}) => {
+    const classes = useStyles();  
+    const [data, setData] = useState(results); 
     return (
         <div className={classes.root}>
-        <Grid container spacing={2}>
-                {
-                    results && results.map((project: any) => (
-                        <Grid item xs={6} sm={3}>
-                        <ProjectCard key={project.id} data={project} />
-                        </Grid>
-                    ))
-                }
-        </Grid>
+          <Grid container spacing={2}>
+            <Grid item xs={1}>
+              <Button variant="contained" color="primary" onClick={() => { setData(sortArray(data, 'asc')) }}>ASC</Button>
+              <Button variant="contained" color="primary" onClick={() => { setData(sortArray(data, 'desc')) }}>DESC</Button>
+            </Grid>
+            <Grid item xs={11}>
+              <Grid container spacing={2}>
+                  {
+                      data && data.map((project: any) => (
+                          <Grid item xs={6} sm={3}>
+                          <ProjectCard key={project.id} data={project} />
+                          </Grid>
+                      ))
+                  }
+              </Grid>
+            </Grid>
+          </Grid>        
         </div>
     );
 }
