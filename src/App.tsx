96754c39cf1ca11dfb08d04a0b48d0516bdc5fb3
Ghania <Ghania@GHANIA>
2020-08-24 23:18:59 +0200

    Change projectsview path

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ProjectsView from './components/ProjectsView';
+import ProjectsView from './views/ProjectsView';
 
 function App() {
   return (
